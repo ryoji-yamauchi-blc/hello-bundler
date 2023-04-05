020d9c7128832d3644577cd04dfbd83fcd87e356
ryoji-yamauchi-blc <ryoji.yamauchi.ut@ambl.co.jp>
2023-04-05 11:20:35 +0900

    script整備

--- src/__tests__/bundler.test.ts
+++ src/__tests__/bundler.test.ts
@@ -4,9 +4,23 @@ import { bundle } from "../bundler";
 const entryFileName = "src/__tests__/target/basic/entry.js";
 
 describe("bundler", () => {
-  test("foo", () => {
+  test("想定どおりのバンドル結果になること", () => {
     const allFiles = getAllDependencies(entryFileName);
     const bundled = bundle(allFiles);
-    console.log(bundled);
+
+    expect(bundled).toEqual(
+      `const baz$4 = () => {
+  console.log("im baz");
+};
+const bar$3 = () => {
+  baz$4();
+  console.log("im baz");
+};
+const foo$2 = () => {
+  console.log("im foo");
+};
+foo$2();
+bar$3();`
+    );
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-export const run = () => {
-  const entryPoint = process.argv[2] as string;
-  if (!entryPoint) throw new Error("エントリーポイントを指定してね。");
+import { writeFileSync } from "fs";
+import { getAllDependencies } from "./graph";
+import { bundle } from "./bundler";
+
+const main = () => {
+  const entryFileName = process.argv[2] as string;
+  if (!entryFileName)
+    throw new Error("エントリーポイントとなるファイル名を指定してね。");
+  const allFiles = getAllDependencies(entryFileName);
+  const bundled = bundle(allFiles);
+  writeFileSync("bundled.js", bundled, "utf8");
 };
+
+main();
